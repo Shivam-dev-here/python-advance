2efcfb584b4b713d43e8146d0ad0896dff6ecf28
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-07-03 14:59:05 +0530

    Docs: why threadPoolExecutor

--- 04-Multi Threading And Multi Processing/03-threadpoolexecutor.py
+++ 04-Multi Threading And Multi Processing/03-threadpoolexecutor.py
@@ -1,5 +1,17 @@
 # Multithreading With Thread Pool Executor
+# Use: Automatic handles (create, start, join) threads also reusable
 
 from concurrent.futures import ThreadPoolExecutor
 import time
 
+def print_quad(val):
+    time.sleep(1)
+    return f"Quad: {val*val*val*val}"
+
+val = [233,1232,131231,1322,1233,12333,133331,13333,111166,1234]
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+    results = executor.map(print_quad, val)
+
+for result in results:
+    print(result)
