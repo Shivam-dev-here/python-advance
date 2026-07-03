ea402e51f02a8b4f0e2c7bfcab7c7a48647264d7
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-07-03 15:46:36 +0530

    feat: Fetching web pages concurently and counting words

--- 04-Multi Threading And Multi Processing/06-real-world-example.py
+++ 04-Multi Threading And Multi Processing/06-real-world-example.py
@@ -7,3 +7,25 @@ Solution: Multithreading allows multiple webpages fetched concurently
 import threading
 import requests
 from bs4 import BeautifulSoup
+
+urls=[
+    'https://portfolio-shivamdevhere.vercel.app/', 
+    'https://www.langchain.com/',
+]
+
+def fetch_content(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print(f'Fetched {len(soup.text)} characters from {url}')
+
+threads = []
+
+for url in urls:
+    thread = threading.Thread(target=fetch_content, args=(url,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+print("All Web Pages Fetched")
