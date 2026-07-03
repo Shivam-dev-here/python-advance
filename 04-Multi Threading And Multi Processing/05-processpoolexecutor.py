abbccf915bda60c8a0e596f924517eff19e34af0
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-07-03 15:06:31 +0530

    feat: ProcessPoolExecutor

--- 04-Multi Threading And Multi Processing/05-processpoolexecutor.py
+++ 04-Multi Threading And Multi Processing/05-processpoolexecutor.py
@@ -3,3 +3,16 @@
 
 from concurrent.futures import ProcessPoolExecutor
 import time
+
+def print_pent(val):
+    time.sleep(1)
+    return f"Quad: {val*val*val*val*val}"
+
+val = [233,1232,131231,1322,1233,12333,133331,13333,111166,1234]
+
+if __name__=="__main__":
+    with ProcessPoolExecutor(max_workers=3) as executor:
+        results = executor.map(print_pent, val)
+
+    for result in results:
+        print(result)
